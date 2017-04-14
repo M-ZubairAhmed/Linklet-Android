653b1476344e1dfed310b2b26df88bcbbdf00369
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-04-14 23:48:22 +0530

    http api interface

---
+++ app/src/main/java/com/fcchyd/linkletandroid/ApiEndPoints.java
@@ -0,0 +1,8 @@
+package com.fcchyd.linkletandroid;
+
+class ApiEndPoints {
+
+    static final String END_POINT_ALL = "\"/api/links/all/\"";
+
+    static final String END_POINT_FILTER = "\"/api/links/filter/\"";
+}
